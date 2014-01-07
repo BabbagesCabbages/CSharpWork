3fce44fef176fca4edee327c946e19da6e509a62
Karson Madden <karsonmadden@gmail.com>
2014-01-07 12:38:42 -0800

    second commit

---
+++ TrialOfHermesGame/AnimatedSprite.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame10
+{
+    class AnimatedSprite
+    {
+        KeyboardState currentKBState;
+        KeyboardState previousKBState;
+
+
+        Texture2D spriteTexture;
+        float timer = 0f;
+        float interval = 200f;
+        int currentFrame = 0;
+        int spriteWidth = 32;
+        int spriteHeight = 48;
+        int spriteSpeed = 2;
+        Rectangle sourceRect;
+        Vector2 position;
+        Vector2 origin;
+
+
+        public AnimatedSprite(Texture2D texture, int currentFrame, int spriteWidth, int spriteHeight)
+        {
+            this.spriteTexture = texture;
+            this.currentFrame = currentFrame;
+            this.spriteWidth = spriteWidth;
+            this.spriteHeight = spriteHeight;
+        }
+
+
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Vector2 Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+
+        public Texture2D Texture
+        {
+            get { return spriteTexture; }
+            set { spriteTexture = value; }
+        }
+
+        public Rectangle SourceRect
+        {
+            get { return sourceRect; }
+            set { sourceRect = value; }
+        }
+
+        public void HandleSpriteMovement(GameTime gameTime)
+        {
+            previousKBState = currentKBState;
+            currentKBState = Keyboard.GetState();
+            
+            sourceRect = new Rectangle(currentFrame * spriteWidth, 0, spriteWidth, spriteHeight);
+
+            if (currentKBState.GetPressedKeys().Length == 0)
+            {
+                if (currentFrame > 1 && currentFrame < 8)
+                {
+                    currentFrame = 1;
+                }
+                if (currentFrame > 9 && currentFrame < 17)
+                {
+                    currentFrame = 9;
+                }
+                if (currentFrame > 17 && currentFrame < 24)
+                {
+                    currentFrame = 17;
+                }
+                if (currentFrame > 25 && currentFrame < 31)
+                {
+                    currentFrame = 26;
+                }
+            }
+
+            // This check is a little bit I threw in there to allow the character to sprint.
+            if (currentKBState.IsKeyDown(Keys.Space))
+            {
+                spriteSpeed = 3;
+                interval = 100;
+            }
+            else
+            {
+                spriteSpeed = 2;
+                interval = 200;
+            }
+
+            if (currentKBState.IsKeyDown(Keys.Right) == true)
+            {
+                AnimateRight(gameTime);
+                if (position.X < 780)
+                {
+                    position.X += spriteSpeed;
+                }
+            }
+
+            if (currentKBState.IsKeyDown(Keys.Left) == true)
+            {
+                AnimateLeft(gameTime);
+                if (position.X > 20)
+                {
+                    position.X -= spriteSpeed;
+                }
+            }
+
+            if (currentKBState.IsKeyDown(Keys.Down) == true)
+            {
+                AnimateDown(gameTime);
+                if (position.Y < 575)
+                {
+                    position.Y += spriteSpeed;
+                }
+            }
+
+            if (currentKBState.IsKeyDown(Keys.Up) == true)
+            {
+                AnimateUp(gameTime);
+                if (position.Y > 25)
+                {
+                    position.Y -= spriteSpeed;
+                }
+            }
+
+            origin = new Vector2(sourceRect.Width / 2, sourceRect.Height / 2);
+        }
+
+        public void AnimateRight(GameTime gameTime)
+        {
+            if (currentKBState != previousKBState)
+            {
+                currentFrame = 9;
+            }
+
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (timer > interval)
+            {
+                currentFrame++;
+
+                if (currentFrame > 11)
+                {
+                    currentFrame = 8;
+                }
+                timer = 0f;
+            }
+        }
+
+        public void AnimateUp(GameTime gameTime)
+        {
+            if (currentKBState != previousKBState)
+            {
+                currentFrame = 25;
+            }
+
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (timer > interval)
+            {
+                currentFrame++;
+
+                if (currentFrame > 30)
+                {
+                    currentFrame = 25;
+                }
+                timer = 0f;
+            }
+        }
+
+        public void AnimateDown(GameTime gameTime)
+        {
+            if (currentKBState != previousKBState)
+            {
+                currentFrame = 17;
+            }
+
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (timer > interval)
+            {
+                currentFrame++;
+
+                if (currentFrame > 23)
+                {
+                    currentFrame = 17;
+                }
+                timer = 0f;
+            }
+        }
+
+        public void AnimateLeft(GameTime gameTime)
+        {
+            if (currentKBState != previousKBState)
+            {
+                currentFrame = 1;
+            }
+
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (timer > interval)
+            {
+                currentFrame++;
+
+                if (currentFrame > 7)
+                {
+                    currentFrame = 4;
+                }
+                timer = 0f;
+            }
+        }
+        
+
+
+    }
+}
